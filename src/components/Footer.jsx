532d392de93bfe74bb4cc685b31d0690ca3cdc09
Tanzim <tanzimr@mun.ca>
2026-03-18 00:18:33 -0230

    chore: clear existing components to start project from scratch

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-black py-6 text-center text-sm text-zinc-100">
-      © {new Date().getFullYear()} Tanzim | Built with React & Tailwind
-    </footer>
-  );
-};
-
-export default Footer;
