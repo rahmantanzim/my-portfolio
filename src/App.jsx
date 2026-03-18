86cfc6f06173a9ec100af02a4c1f8cec35d12f23
Tanzim <tanzimr@mun.ca>
2026-03-18 00:52:13 -0230

    ui: setup initial folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import { useState } from 'react'
-import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import Navbar from '@/layout/Navbar'
 function App() {
   return (
-    <>
-    {/* Home Page 
-      componetns:
-      navbar 
-      footer
-      header
-      Projects
-      contact
-    About Page */}
-    <Routes>
-      
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>}/>
-    </Routes>    
-    </>
+    <div className="min-h-screen overfow-x-hidden">
+      <Navbar/>
+      <main>
+
+      </main>
+    </div>
   )
 }
 
